7bb9f9a9763cba35035cfbf30fbbb33ab7f17ac0
James McGraw <james.mcgraw.allen@gmail.com>
2023-06-14 09:13:19 +0100

    added planets exercise (unfinished)

---
+++ planets-exercise/js/index.js
@@ -0,0 +1,27 @@
+"use strict";
+const submitButton = document.querySelector('input');
+const submittedAge = document.querySelector('input').value;
+const textToEdit = document.querySelector('.answers');
+const mercury = 0.2408467;
+const venus = 0.61519726;
+const mars = 1.8808158;
+const jupiter = 11.862615;
+const saturn = 29.447498;
+const uranus = 84.016846;
+const neptune = 164.79132;
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("pressed submit");
+    textToEdit.innerHTML =
+        "On Mercury you are " + (submittedAge * mercury);
+    "On Venus you are " +
+        "On Mars you are " +
+        "On Jupiter you are " +
+        "On Saturn you are " +
+        "On Uranus you are " +
+        "On Neptune you are " +
+    ;
+};
+if (button) {
+    submitButton.addEventListener('submit', handleSubmit);
+}
---
+++ planets-exercise/ts/index.ts
@@ -0,0 +1,30 @@
+const submitButton: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>('button');
+const submittedAge: number | null = document.querySelector('input').value
+const textToEdit: HTMLDivElement = document.querySelector('.answers')
+
+const mercury: number = 0.2408467;
+const venus: number = 0.61519726;
+const mars: number = 1.8808158;
+const jupiter: number = 11.862615 ;
+const saturn: number = 29.447498;
+const uranus: number = 84.016846;
+const neptune: number = 164.79132;
+
+
+const handleSubmit = (e: SubmitEvent): void => {
+    e.preventDefault()
+    console.log("pressed submit")
+    textToEdit.innerHTML =
+    "On Mercury you are " + (submittedAge * mercury)
+    "On Venus you are " +
+    "On Mars you are " +
+    "On Jupiter you are " +
+    "On Saturn you are " +
+    "On Uranus you are " +
+    "On Neptune you are " +
+
+}
+
+if (button) {
+    submitButton.addEventListener('submit', handleSubmit)
+}
